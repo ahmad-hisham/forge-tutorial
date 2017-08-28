be9ecfb2dbd4cde8b771d5e271415c6f56dd2e1a
Ahmad Hisham <ahmad.hisham@gmail.com>
2017-08-28 01:36:12 +0000

    Change name of ForgeSample model to ForgeUploadSample

--- app/controllers/upload_sample_controller.rb
+++ app/controllers/upload_sample_controller.rb
@@ -1,7 +1,7 @@
 class UploadSampleController < ApplicationController
   def run
-    forge_sample_api = ForgeSample.new
-    response = forge_sample_api.upload_parse_link_file
+    forge_upload_sample_api = ForgeUploadSample.new
+    response = forge_upload_sample_api.upload_parse_link_file
     @text = response.gsub("\n", "<br/>").html_safe
   end
 end
