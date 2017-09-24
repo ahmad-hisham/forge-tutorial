9a113f45b06b63b9cd0bbab0aee1c43de69cd370
Ahmad Hisham <ahmad.hisham@gmail.com>
2017-09-24 08:39:56 +0000

    New button in viewer toolbar extension to list all 3D objects in active model in Console log

--- public/viewer-extension-in-toolbar.js
+++ public/viewer-extension-in-toolbar.js
@@ -47,10 +47,17 @@ class MyViewerExtentionInToolbar extends Autodesk.Viewing.Extension {
     button2.addClass('my-view-unlock-button');
     button2.setToolTip('Unlock View');
 
+    // Button 3
+    var button3 = new Autodesk.Viewing.UI.Button('my-view-list-objects-button');
+    button3.onClick = () => {this.listObjects()};
+    button3.addClass('my-view-list-objects-button');
+    button3.setToolTip('List all Objects');
+
     // SubToolbar
     this.subToolbar = new Autodesk.Viewing.UI.ControlGroup('my-extension-view-toolbar');
     this.subToolbar.addControl(button1);
     this.subToolbar.addControl(button2);
+    this.subToolbar.addControl(button3);
   
     viewer.toolbar.addControl(this.subToolbar);
   }
@@ -61,6 +68,47 @@ class MyViewerExtentionInToolbar extends Autodesk.Viewing.Extension {
 
   unlockViewport() {
     this.viewer.setNavigationLock(false);
+  }
+
+  listObjects() {
+    this.modelTree = this.buildModelTree(this.viewer.model);
+
+    console.log(this.viewer.model);
+    console.log(this.modelTree);
+    alert("Extracted info on " + this.modelTree.children.length + " items from the model");
+  }
+
+  buildModelTree(model){
+
+    //builds model tree recursively
+    function _buildModelTreeRec(node){
+      instanceTree.enumNodeChildren(node.dbId, (childId) => {
+          node.children = node.children || [];
+
+          var childNode = {
+            dbId: childId,
+            name: instanceTree.getNodeName(childId)
+          };
+
+          node.children.push(childNode);
+
+          _buildModelTreeRec(childNode);
+        });
+    }
+
+    //get model instance tree and root component
+    var instanceTree = model.getData().instanceTree;
+
+    var rootId = instanceTree.getRootId();
+
+    var rootNode = {
+      dbId: rootId,
+      name: instanceTree.getNodeName(rootId)
+    };
+
+    _buildModelTreeRec(rootNode);
+
+    return rootNode;
   }
 }
 
